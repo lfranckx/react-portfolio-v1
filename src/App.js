9dcead1a20074dde99fd88d3e558dc57f680fdb0
Bowie <lfranckx@thinkful.com>
2021-01-30 08:58:43 -0600

    add new pages

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './components/styles/App.css';
 
+import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
+
 
 import './App.css';
 
 function App() {
   return (
-    <>
+    <Router>
 
-    </>
+    </Router>
   );
 }
 
