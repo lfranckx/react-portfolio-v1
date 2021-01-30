6abc659a141077b51f3b51c0bfe6e90c6ca3a105
Bowie <lfranckx@thinkful.com>
2021-01-30 09:35:43 -0600

    fix file import routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './components/styles/App.css';
+import './styles/App.css';
 
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
