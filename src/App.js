80531d69aaef6f6409e14bf534b2d7fe559e215f
Bowie <lfranckx@thinkful.com>
2021-01-30 09:08:17 -0600

    add imports

--- src/App.js
+++ src/App.js
@@ -7,16 +7,22 @@ import Home from './components/pages/Home';
 import Portfolio from './components/pages/Portfolio';
 import Resume from './components/pages/Resume';
 import About from "./components/pages/About";
-
-
-
-import './App.css';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
     <Router>
       <Switch>
-
+        <Navbar />
+        <main>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/portfolio' component={Portfolio} />
+            <Route path='/resume' component={Resume} />
+            <Route path='/contact' component={Contact} />
+            <Route path={'/about'} component={About}/>
+          </Switch>
+        </main>
       </Switch>
     </Router>
   );
